14de4c5f15d5c678bc1bf0a9925ac9a5bb96f0a0
Your Name <1240201765@qq.com>
2021-04-22 15:48:11 +0800

    卷积神经网络用于mnist数据集

--- d_basic_cnn/convolution2.py
+++ d_basic_cnn/convolution2.py
@@ -12,8 +12,8 @@ input = [
     3,7,6,4,1
 ]
 input = torch.Tensor(input).view(1,1,5,5) # batch chanel width height
-
-conv_layer = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,padding = 1,bias = False)
+# kernel_size 卷积核大小 padding几个外圈填充  stride卷积核移动步长（默认1）
+conv_layer = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,padding = 1,stride=2,bias = False)
 
 kernel = torch.Tensor([1,2,3,4,5,6,7,8,9]).view(out_channels,in_channels,kernel_size,kernel_size) #out in w h
 conv_layer.weight.data = kernel.data
